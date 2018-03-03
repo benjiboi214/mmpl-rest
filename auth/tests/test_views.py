5ac64004244cf0a3eea0a508984f7263af2a91f4
Ben Elliot <benjiboi214@gmail.com>
2018-03-03 16:18:10 +1100

    404 on the user delete endpoint.

---
+++ auth/tests/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+# Create your tests here.
+class DeleteViewTest(TestCase):
+    
+    def test_returns_404(self):
+        response = self.client.get('/auth/users/delete/')
+        self.assertEqual(404, response.status_code)
