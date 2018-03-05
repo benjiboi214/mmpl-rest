dbc1a3cdc3ec190ac5ac80ac29234b2ab118742f
Ben Elliot <benjiboi214@gmail.com>
2018-03-05 17:58:43 +1100

    Split testing out to a base file, cleaned up imports.

--- functional_tests/test_auth_api.py
+++ functional_tests/test_auth_api.py
@@ -1,65 +1,12 @@
 import re
-import time
-from unittest import skip
 
-from django.contrib.auth import get_user_model
-from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.core import mail
 from django.test.utils import override_settings
-from rest_framework.test import APIClient
-from selenium import webdriver
+
+from .base import FunctionalRestTest
 
 
-class FunctionalTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-    
-    def tearDown(self):
-        self.browser.quit()
-    
-    @skip
-    @override_settings(DEBUG=True)
-    def test_django_installed(self):
-        self.browser.get(self.live_server_url)
-        time.sleep(4)
-        self.assertIn('Django', self.browser.title)
-
-class FunctionalRESTTest(StaticLiveServerTestCase):
-    def setUp(self):
-        self.client = APIClient()
-        self.other_user = {
-            'email': "otheruser@mail.com",
-            'name': "Other User",
-            'password': "Password01",
-        }
-        test_user = get_user_model().objects.create_user(self.other_user['email'], password=self.other_user['password'])
-        test_user.name = self.other_user['name']
-        test_user.save()
-    
-    def create_jwt(self, email, password):
-        return self.client.post(
-            '/auth/jwt/create/',
-            {
-                'email': email,
-                'password': password
-            },
-            format='json'
-        )
-    
-    def get_uid_and_token_from_email(self, regex, email_index):
-        email = mail.outbox[email_index]
-        search = re.search(regex, email.body)
-        uid = search.group(1)
-        token = search.group(2)
-        return (uid, token)
-
-    def check_me_endpoint(self, user_name, user_jwt):
-        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + user_jwt)
-        response = self.client.get(
-            '/auth/me/',
-        )
-        self.assertEquals(200, response.status_code)
-        self.assertEquals(user_name, response.data['name'])
+class AuthFunctionalTests(FunctionalRestTest):
 
     def test_user_can_register_and_login(self):
         user = {
