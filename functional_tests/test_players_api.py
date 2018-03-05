db2f83332edd3686adcab192043713cff02977c3
Ben Elliot <benjiboi214@gmail.com>
2018-03-05 22:56:12 +1100

    Written failing functional test, moving on to unit tests.

---
+++ functional_tests/test_players_api.py
@@ -0,0 +1,24 @@
+# User can edit, update and otherwise change their player profile.
+# User cannot edit another players profile.
+# If a profile exists without an associated profile, player can claim the profile
+# Admin should recieve confirmation that profile has been claimed awaiting approval.
+# User can input details such as contact details, preferred contact method, address, umpire accreditation.
+# User can enter a preferred Name (screen name?)
+
+from .base import FunctionalRestTest
+
+
+class PlayerFunctionalTests(FunctionalRestTest):
+
+    def test_player_profile_can_be_edited(self):
+        # User logs in
+        response = self.create_jwt(self.other_user['email'], self.other_user['password'])
+        self.other_user['jwt'] = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.other_user['jwt'])
+
+        # Once logged in, user has access to their player profile
+        response = self.client.get(
+            '/players/me/',
+            format='json'
+        )
+        self.assertEqual(200, response.status_code)
