30c97d970d6723a130fc099e5f41aefded021579
Ben Elliot <benjiboi214@gmail.com>
2018-03-10 15:59:13 +1100

    Passed functional test! - Added tests for get and put methods on view. - Wrote get and put methods on view.

--- userprofile/tests/test_views.py
+++ userprofile/tests/test_views.py
@@ -18,18 +18,26 @@ class TestPlayerView(TestCase):
             password=self.user_details['password'],
             name=self.user_details['name']
         )
-        self.factory = APIRequestFactory()
-        self.view = MyProfileDetail.as_view()
+        self.user.profile.address = '123 Fake St'
+        self.user.profile.save()
+    
+    def get_view_response(self, action, data=None, authenticate=False):
+        factory = APIRequestFactory()
+        view = MyProfileDetail.as_view()
+        if data:
+            request = getattr(factory, action)('/profiles/me', data, format='json')
+        else:
+            request = getattr(factory, action)('/profiles/me')
+        if authenticate:
+            force_authenticate(request, self.user)
+        return view(request)
 
     def test_view_available_to_authenticated_user(self):
-        request = self.factory.get('/profiles/me')
-        force_authenticate(request, self.user)
-        response = self.view(request)
+        response = self.get_view_response('get', authenticate=True)
         self.assertEqual(200, response.status_code)
 
     def test_view_not_available_to_unauthenticated_user(self):
-        request = self.factory.get('/profiles/me')
-        response = self.view(request)
+        response = self.get_view_response('get')
         self.assertEqual(401, response.status_code)
     
     def test_get_profile_method_success(self):
@@ -40,3 +48,23 @@ class TestPlayerView(TestCase):
         user = get_user_model().objects.create()
         user_profile = MyProfileDetail.get_player_object(self, user)
         self.assertEqual(user.profile, user_profile)
+    
+    def test_get_returns_correct_profile(self):
+        response = self.get_view_response('get', authenticate=True)
+        self.assertEqual(200, response.status_code)
+        self.assertEqual(self.user.profile.address, response.data['address'])
+
+    def test_put_updates_profile(self):
+        new_address = '321 Ekaf Ts'
+        response = self.get_view_response('put', data={'address': new_address}, authenticate=True)
+        self.assertEqual(new_address, response.data['address'])
+    
+    def test_put_invalid_data_throws_error(self):
+        long_address = '''
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque tincidunt neque quis
+        ullamcorper blandit. Pellentesque sit amet pharetra dolor. Sed in posuere velit. Curabitur
+        eu auctor magna. Blah Blah.o
+        '''
+        response = self.get_view_response('put', data={'address': long_address}, authenticate=True)
+        self.assertEqual(400, response.status_code)
+        self.assertIn('no more than 200 characters.', response.data['address'][0])
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,11 @@
 from django.http import Http404, HttpResponse
 from django.shortcuts import render
-from rest_framework import permissions
+from rest_framework import permissions, status
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from userprofile.models import Profile
+from userprofile.serializers import ProfileSerializer
 
 
 class MyProfileDetail(APIView):
@@ -17,7 +19,14 @@ class MyProfileDetail(APIView):
             raise Http404
 
     def get(self, request, format=None):
-        return HttpResponse("Here is your response testing goat!")
+        profile = self.get_player_object(request.user)
+        serializer = ProfileSerializer(profile)
+        return Response(serializer.data)
 
     def put(self, request, format=None):
-        return HttpResponse("Here is your response testing goat!")
+        profile = self.get_player_object(request.user)
+        serializer = ProfileSerializer(profile, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
