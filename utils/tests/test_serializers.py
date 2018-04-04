a2a337a5b38165b6cb81393b5f174800c9bb60d6
Ben Elliot <benjiboi214@gmail.com>
2018-04-05 08:24:55 +1000

    100% Test coveragegit status

--- utils/tests/test_serializers.py
+++ utils/tests/test_serializers.py
@@ -5,19 +5,25 @@ from utils.serializers import ChoiceDisplayField, DefaultModelSerializer
 
 class SerializerUtilTest(TestCase):
 
-    def test_choice_field_shows_value_and_display(self):
-        CHOICES = (
+    def setUp(self):
+        self.CHOICES = (
             ('A', 'A Display'),
             ('B', 'B Display')
         )
-        field = ChoiceDisplayField(CHOICES)
+
+    def test_choice_field_shows_value_and_display(self):
+        field = ChoiceDisplayField(self.CHOICES)
         self.assertEqual({
-            'value': CHOICES[0][0],
-            'display': CHOICES[0][1]
-        }, field.to_representation(CHOICES[0][0]))
+            'value': self.CHOICES[0][0],
+            'display': self.CHOICES[0][1]
+        }, field.to_representation(self.CHOICES[0][0]))
 
     def test_choices_field_set_on_serializer_model(self):
         serializer = DefaultModelSerializer()
         self.assertEqual(
             ChoiceDisplayField,
             serializer.serializer_choice_field)
+
+    def test_none_return_on_custom_field(self):
+        field = ChoiceDisplayField(self.CHOICES)
+        self.assertEqual(None, field.to_representation(None))
